f0fe1691b97edf614f81d1ba94c6375912ce5e28
AhmedSajid1506 <jujwuth2904@gmail.com>
2023-04-19 15:44:48 +0500

    How It Works Complete

--- client/src/components/HowItWorks.js
+++ client/src/components/HowItWorks.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../css/howitworks.css';
 import hiw1 from "../img/hiw-first.png";
 import hiw2 from "../img/hiw-second.png";
 import hiw3 from "../img/hiw-third.png";
@@ -8,28 +9,32 @@ const HowItWorks = () => {
   return (
     <div className="text-center">
       <h2 className="f2 f-primary">How It Works</h2>
-      <div className="row">
-        <div className="col-md-3">
-          <div className="h-100 bg-white d-flex flex-column align-items-center justify-content-between">
-            <img className="w-50" src={hiw1} alt="HIW First Step" />
+      <div className="row my-4">
+        <div className="col-md-3 mb-md-0 mb-4">
+          <div className="h-100 bg-white d-flex flex-column align-items-center justify-content-between position-relative p-3">
+            <div className="bg-main hiw-nums">1</div>
+            <img className="w-50 mb-3" src={hiw1} alt="HIW First Step" />
             <h5>Signup / Login</h5>
           </div>
         </div>
-        <div className="col-md-3">
-          <div className="h-100 bg-white d-flex flex-column align-items-center justify-content-between">
-            <img className="w-50" src={hiw2} alt="HIW Second Step" />
+        <div className="col-md-3 mb-md-0 mb-4">
+          <div className="h-100 bg-white d-flex flex-column align-items-center justify-content-between position-relative p-3">
+            <div className="bg-main hiw-nums">2</div>
+            <img className="w-50 mb-3" src={hiw2} alt="HIW Second Step" />
             <h5>See Packages</h5>
           </div>
         </div>
-        <div className="col-md-3">
-          <div className="h-100 bg-white d-flex flex-column align-items-center justify-content-between">
-            <img className="w-50" src={hiw3} alt="HIW Third Step" />
+        <div className="col-md-3 mb-md-0 mb-4">
+          <div className="h-100 bg-white d-flex flex-column align-items-center justify-content-between position-relative p-3">
+            <div className="bg-main hiw-nums">3</div>
+            <img className="w-50 mb-3" src={hiw3} alt="HIW Third Step" />
             <h5>Give Order</h5>
           </div>
         </div>
-        <div className="col-md-3">
-          <div className="h-100 bg-white d-flex flex-column align-items-center justify-content-between">
-            <img className="w-50" src={hiw4} alt="HIW Forth Step" />
+        <div className="col-md-3 mb-md-0 mb-4">
+          <div className="h-100 bg-white d-flex flex-column align-items-center justify-content-between position-relative p-3">
+            <div className="bg-main hiw-nums">4</div>
+            <img className="w-50 mb-3" src={hiw4} alt="HIW Forth Step" />
             <h5>Get Your Order</h5>
           </div>
         </div>
